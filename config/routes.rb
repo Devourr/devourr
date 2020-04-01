fc605d2a927cc692ba9fdaac5eecf3e5da442a1f
Pat <patrick.h.needham@gmail.com>
2020-04-01 15:12:39 -0700

    added redirect so www is not used

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # redirect www to non-www
+  # https://coderwall.com/p/l1uydg/redirect-to-canonical-host-in-rails-4
+  if ENV['CANONICAL_HOST'] && !(Rails.env.development? || Rails.env.test?)
+    constraints(host: /^(?!#{ENV['CANONICAL_HOST'].gsub('.', '\.')})/i) do
+      match '/(*path)' => redirect {
+        |params, req| "https://#{ENV['CANONICAL_HOST']}/#{params[:path]}"
+      }, via: [:get, :post]
+    end
+  end
+
   root 'main#index'
 
 end
