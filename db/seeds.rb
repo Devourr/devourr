4247ba160cdbe6738ee7da65161ce3370e66b214
Pat <patrick.h.needham@gmail.com>
2020-04-01 22:35:21 -0700

    added user class from devise with seed user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(first_name: 'Pat', last_name: 'Needham', email: ENV['DEVELOPER_EMAIL'], password: 'password', password_confirmation: 'password')
