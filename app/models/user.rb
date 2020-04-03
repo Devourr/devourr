d16a3365e283498df188f440b636f94da21b5fca
Pat <patrick.h.needham@gmail.com>
2020-04-02 17:53:13 -0700

    added model validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :confirmable, :lockable, :timeoutable, :trackable
 
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
+  before_save :downcase_email
+
+  # make emails case insensitive
+  def downcase_email
+    self.email = email.downcase
+  end
 end
